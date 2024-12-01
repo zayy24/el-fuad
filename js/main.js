25526a27397c33560c5d7ae8a84b99c5aad05d51
zayy24 <puadizax@gmail.com>
2024-12-01 10:34:06 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('WOPYU SAYANG KU YUNIAR').split('')
+    const titles = ('WOPYU SAYANGKU').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
